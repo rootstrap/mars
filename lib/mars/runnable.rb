c733b153dace43ba2e6a5db5a5c6dcc5fa7ee48a
Andres Garcia <andres@rootstrap.com>
2025-09-24 18:01:53 -0300

    Add Runnable and Sequential workflow (#1)
    * Add Runnable and Sequential workflow

---
+++ lib/mars/runnable.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Mars
+  class Runnable
+    def run(input)
+      raise NotImplementedError
+    end
+  end
+end
---
+++ lib/mars/workflows/sequential.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative "../runnable"
+
+module Mars
+  module Workflows
+    class Sequential < Runnable
+      def initialize(name, steps:)
+        @name = name
+        @steps = steps
+      end
+
+      def run(input)
+        @steps.each do |step|
+          input = step.run(input)
+        end
+
+        input
+      end
+    end
+  end
+end
