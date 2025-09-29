465e49de5d96460970cc61e5b1e90456d1ead6b6
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2025-09-29 10:58:45 -0300

    Add Zeitwerk (#4)

--- lib/mars.rb
+++ lib/mars.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "mars/version"
-require_relative "mars/runnable"
-require_relative "mars/workflows/sequential"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module Mars
   class Error < StandardError; end
-  # Your code goes here...
 end
